7c137bcd33b44d95a37a8479378ca752f1c439a0
indigo <indigoprogrammer@outlook.com>
2025-11-21 10:32:49 -0700

    Landing page update
    Added a couple of sections to the landing page

--- app.py
+++ app.py
@@ -5,7 +5,18 @@ app = Flask(__name__)
 
 @app.route("/")
 def index() -> str:
-    render_template("index.html")
+    return render_template("index.html")
+
+
+@app.route("/about-us")
+def aboutUs() -> str:
+    return render_template("aboutus.html")
+
+
+# Expos
+@app.route("/exposiciones/avant-garde")
+def expoAvantGarde() -> str:
+    return render_template("articles/avant-garde.html")
 
 
 if __name__ == "__main__":
